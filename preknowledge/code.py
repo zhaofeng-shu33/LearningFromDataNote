f55b21f1c2598bbeb711b54d09070e81fa992103
zhaofeng-shu33 <616545598@qq.com>
2020-09-05 10:15:03 +0800

    update code

--- preknowledge/code.py
+++ preknowledge/code.py
@@ -8,6 +8,8 @@ print(np.linalg.eig(A)[0])
 A = np.array([[1, 2], [3, 4], [5, 6]])
 print(np.sum(A, axis=1))
 
+print(A @ np.array([1, 1]))
+
 import scipy.stats
 x = np.linspace(-3, 3)
 y = scipy.stats.norm.pdf(x)
